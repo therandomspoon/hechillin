7d3022c5f17b9b40ec4ff4b1634b268dcc4eceb0
therandomspoon <107148755+therandomspoon@users.noreply.github.com>
2023-03-24 20:46:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Get the toggle switch and video elements
 const toggleSwitch = document.querySelector('#toggle');
 const video = document.querySelector('#myVideo');
 
-// Add event listener to toggle switch
 toggleSwitch.addEventListener('change', toggleDarkMode);
 
-// Function to toggle dark mode
 function toggleDarkMode() {
   if (toggleSwitch.checked) {
     document.body.classList.add('dark-mode');
@@ -14,17 +11,28 @@ function toggleDarkMode() {
   }
 }
 
-// Add event listener to video for timeupdate event
-video.addEventListener('timeupdate', function() {
-  const currentTime = video.currentTime;
-  const hours = new Date().getHours();
-  const minutes = new Date().getMinutes();
 
-  // Check if time is between 8:12 PM and 8:13 PM
-  if (hours === 8 && minutes === 12 && currentTime >= 265 && currentTime < 266) {
-    video.pause();
-    setTimeout(function() {
-      video.play();
-    }, 60000); // Wait for 1 minute before resuming playback
+// Set the video to autoplay and loop
+video.autoplay = true;
+video.loop = true;
+
+function playVideoAt8AM() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const currentTime = video.currentTime;
+
+  if (hours === 8 && minutes === 0) {
+    video.play();
   }
-});
+
+  if (hours === 8 && minutes === 12 && currentTime >= 252 && currentTime < 253) {
+    video.pause();
+  }
+
+  if (hours === 8 && minutes === 13 && currentTime >= 252 && currentTime < 253) {
+    video.play();
+  }
+}
+
+setInterval(playVideoAt8AM, 1000);
